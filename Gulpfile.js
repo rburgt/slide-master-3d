e17395be287908484405c0030cfc76211e7edf38
Rob van der Burgt <rob.vanderburgt@incentro.com>
2014-12-25 11:01:31 +0100

    Added templating build step for site integration sample

--- Gulpfile.js
+++ Gulpfile.js
@@ -24,4 +24,28 @@ gulp.task('build:asset:scss', function () {
 gulp.task('build:live', function(){
     gulp.watch('./src/asset/scss/**/*.scss', ['build:asset:scss']);
     gulp.watch('./src/asset/javascript/**/*.js', ['build:asset:javascript']);
+
+    gulp.watch('./src/site/template/**/*.html', ['build:site:template']);
+});
+
+
+gulp.task('build:site:template', function () {
+    return gulp.src( './src/site/template/*.html')
+        .pipe($.swig({
+            defaults: {
+                cache: false
+            }
+        }))
+        .on('error', function(error) {
+            console.log(error);
+        })
+        .pipe($.htmltidy({
+            doctype: 'html5',
+            hideComments: false,
+            'fix-bad-comments': false,
+            wrap: 0,
+            indent: true,
+            'indent-spaces': 4
+        }))
+        .pipe(gulp.dest('./build/site'))
 });
