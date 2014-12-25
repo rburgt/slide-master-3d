cfa37f2ff1bf0c316e30c953f5d84cf0597a65a4
Rob van der Burgt <rob.vanderburgt@incentro.com>
2014-12-25 10:49:48 +0100

    Added javascript build task, implements browserify, 6to5 and minification

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+
+gulp.task('build:asset:javascript', function () {
+    browserify('./src/asset/javascript/slide-master-3d.js')
+        .bundle()
+        .pipe(source('slide-master-3d.js'))
+        .pipe($.streamify($.uglify()))
+        .pipe(gulp.dest('./build/javascript'));
+});
 
 gulp.task('build:asset:scss', function () {
-    gulp.src('./src/asset/scss/*.scss')
+    return gulp.src('./src/asset/scss/*.scss')
         .pipe($.rubySass())
         .pipe($.autoprefixer())
         .pipe(gulp.dest('./build/css'));
@@ -12,4 +23,5 @@ gulp.task('build:asset:scss', function () {
 
 gulp.task('build:live', function(){
     gulp.watch('./src/asset/scss/**/*.scss', ['build:asset:scss']);
-})
+    gulp.watch('./src/asset/javascript/**/*.js', ['build:asset:javascript']);
+});
