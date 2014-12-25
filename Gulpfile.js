a46b9c977118dd2a8a60a336891904487710936f
Rob van der Burgt <rob.vanderburgt@incentro.com>
2014-12-25 10:34:52 +0100

    Implemented basic doCSSa setup

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,9 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+
+
+gulp.task('build:asset:scss', function () {
+    gulp.src('./src/asset/scss/*.scss')
+        .pipe($.rubySass())
+        .pipe(gulp.dest('./build/css'));
+});
