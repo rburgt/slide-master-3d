1e52e5909fc31ad6b27325fb0ada7b0e190a7233
Rob van der Burgt <rob.vanderburgt@incentro.com>
2015-01-02 11:17:30 +0100

    Added support for browersync live building

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,50 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
 
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+
+// build all
+gulp.task('build', [
+    'build:asset',
+    'build:site'
+]);
+
+// build, start watch tasks and start browsersync webserver to show build results
+gulp.task('build:serve', ['build', 'build:live'], function(){
+    browserSync.init(null, {
+        server: {
+            baseDir: ['./build']
+        },
+        notify: false,
+        startPath: '/site'
+    });
+
+    gulp.watch('./build/**/*', (function(){
+        var cb;
+        return function(){
+            clearTimeout(cb);
+            cb = setTimeout(reload, 60);
+        };
+    })());
+});
+
+
+gulp.task('build:live', function(){
+    gulp.watch('./src/asset/scss/**/*.scss', ['build:asset:scss']);
+    gulp.watch('./src/asset/javascript/**/*.{js,html}', ['build:asset:javascript']);
+    gulp.watch('./src/site/template/**/*.html', ['build:site:template']);
+});
+
+
+// build all asset tasks
+gulp.task('build:asset', [
+    'build:asset:javascript',
+    'build:asset:scss'
+]);
+
+
 gulp.task('build:asset:javascript', function () {
     browserify('./src/asset/javascript/slide-master-3d.js')
         .bundle()
@@ -29,13 +73,9 @@ gulp.task('build:asset:scss', function () {
 });
 
 
-gulp.task('build:live', function(){
-    gulp.watch('./src/asset/scss/**/*.scss', ['build:asset:scss']);
-    gulp.watch('./src/asset/javascript/**/*.js', ['build:asset:javascript']);
-
-    gulp.watch('./src/site/template/**/*.html', ['build:site:template']);
-});
-
+gulp.task('build:site', [
+    'build:site:template'
+]);
 
 gulp.task('build:site:template', function () {
     return gulp.src( './src/site/template/*.html')
